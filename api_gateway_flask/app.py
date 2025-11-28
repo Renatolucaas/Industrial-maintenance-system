4ede71822eee9aa45518f209ab32cc2cb16d0d95
Renato Lucas <renatolucas@ucl.br>
2025-11-28 10:04:08 -0300

    verificando se as credenciais da aws estão configuradas

--- api_gateway_flask/app.py
+++ api_gateway_flask/app.py
@@ -14,3 +14,8 @@ def detectar_ambiente():
     """
     Detecta automaticamente se estamos em ambiente AWS ou desenvolvimento local
     """
+     # Verifica se as credenciais AWS estão configuradas
+    try:
+        # Tenta acessar o S3
+        s3 = boto3.client('s3')
+        s3.list_buckets()
